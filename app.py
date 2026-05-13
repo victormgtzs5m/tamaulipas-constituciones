048dd7d5f3be6476ebbc919813278ecd873151f9
victormgtzs5m <victorm.gtzs@gmail.com>
2026-05-13 08:41:17 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -490,6 +490,7 @@ def comparative_plot(data, y_col, title, y_title, pozos_sel_comp, semilog=False)
         gridcolor="#EAECEE",
         zeroline=False,
         separatethousands=True
+        tickfont=dict(size=16)
     )
 
     return fig
