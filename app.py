92c3f73ff4ef884b05ad94fc295ffb8026ecfd06
victormgtzs5m <victorm.gtzs@gmail.com>
2026-05-13 08:50:29 -0600

    app.py

--- app.py
+++ app.py
@@ -494,11 +494,7 @@ def comparative_plot(data, y_col, title, y_title, pozos_sel_comp, semilog=False)
         family="Arial Black",
         size=22
         ),
-
-        tickfont=dict(
-        family="Arial Black",
-        size=16
-        ),
+     
         type="log" if semilog else "linear",
         showgrid=True,
         gridcolor="#EAECEE",
