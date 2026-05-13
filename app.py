ff847cd866f7be364123b2cfc40102d9ef28167a
victormgtzs5m <victorm.gtzs@gmail.com>
2026-05-13 11:44:27 -0600

    app.py

--- app.py
+++ app.py
@@ -493,7 +493,7 @@ def comparative_plot(data, y_col, title, y_title, pozos_sel_comp, semilog=False)
     )
 
     fig.update_yaxes(
-        title_text=<b>y_title</b>,
+        title_text=y_title,
         type="log" if semilog else "linear",
         showgrid=True,
         gridcolor="#EAECEE",
